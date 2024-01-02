24100023e346c6615e4a372f7c1134946382750c
srujan <srujanvinodsarode@gmail.com>
2024-01-02 12:42:41 +0530

    gg

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<h1>Srujan Vinod Sarode 1BM21IS411</h1>"
+    return "<h1>Soumya Ranjan Sahoo 1BM21IS410</h1>"
 
 
 if __name__ == "__main__":
